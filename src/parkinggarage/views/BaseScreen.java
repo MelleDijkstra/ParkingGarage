7c4beb2475df6be6716a0aceec6e791500f77011
MelleDijkstra <melle210202@gmail.com>
2017-01-23 14:26:07 +0100

    Created abstract view, so creating a new view is easier

---
+++ src/parkinggarage/views/BaseScreen.java
@@ -0,0 +1,47 @@
+package parkinggarage.views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by melle on 23-1-2017.
+ */
+public abstract class BaseScreen {
+
+    protected Stage mainStage;
+
+    public BaseScreen() throws IOException {
+        mainStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("../layouts/"+getLayoutFile()));
+        mainStage.setTitle(getTitle());
+
+        Scene mainScene = new Scene(root, getWidth(), getHeight());
+        mainScene.getStylesheets().add(getClass().getResource("../resources/css/style.css").toString());
+
+        mainStage.setScene(mainScene);
+    }
+
+    public void show() {
+        mainStage.show();
+    }
+
+    public void hide() {
+        mainStage.hide();
+    }
+
+    public abstract String getLayoutFile();
+
+    public abstract String getTitle();
+
+    public int getWidth() {
+        return 500;
+    }
+
+    public int getHeight() {
+        return 500;
+    }
+}
