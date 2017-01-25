fe8ddf129501dcc1154f4fc43923e118e633f14c
JanD98 <jan.duursma@live.nl>
2017-01-25 11:15:23 +0100

    SettingsScreen fix

--- src/parkinggarage/controllers/MainController.java
+++ src/parkinggarage/controllers/MainController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import parkinggarage.SettingsScreen;
+import parkinggarage.views.SettingsScreen;
 import parkinggarage.Simulation;
 import parkinggarage.views.CreditsScreen;
 
